232e94defe3e380f5f6db6fde811a09268c2142f
septiandr <sdwirisanggalih@gmail.com>
2026-01-07 15:09:17 +0700

    add filter outlets

--- src/services/outlet_services.go
+++ src/services/outlet_services.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fs-regenera/src/model"
 	"os"
+	"sort"
+	"strings"
 )
 
 func GetOutletListServices(ctx context.Context, params model.OutletListParams) ([]model.OutletResponse, int, error) {
@@ -21,5 +23,62 @@ func GetOutletListServices(ctx context.Context, params model.OutletListParams) (
 	if err != nil {
 		return nil, 0, err
 	}
-	return outlets, len(outlets), nil
+
+	filteredOutlets := make([]model.OutletResponse, 0)
+	for _, outlet := range outlets {
+		//filter status
+		if params.Status != "" && outlet.Status != string(params.Status) {
+			continue
+		}
+
+		//filter search ""
+		if params.Search != "" {
+			if !strings.Contains(strings.ToLower(outlet.Name), strings.ToLower(params.Search)) {
+				continue
+			}
+		}
+		filteredOutlets = append(filteredOutlets, outlet)
+	}
+
+	total := len(filteredOutlets)
+
+	//=====Sort===
+
+	if params.SortBy != "" && params.SortType != "" {
+		sort.Slice(filteredOutlets, func(i, j int) bool {
+			var less bool
+			switch params.SortBy {
+			case model.SortByCode:
+				less = filteredOutlets[i].Code < filteredOutlets[j].Code
+			case model.SortByName:
+				less = filteredOutlets[i].Name < filteredOutlets[j].Name
+			case model.SortByCreatedAt:
+				less = filteredOutlets[i].CreatedAt.Before(filteredOutlets[j].CreatedAt)
+			default:
+				less = true
+			}
+			if params.SortType == model.SortDESC {
+				return !less
+			}
+			return less
+		})
+	}
+
+	// =========================
+	// PAGINATION
+	// =========================
+	start := (params.Page - 1) * params.Limit
+	end := start + params.Limit
+
+	if start > total {
+		return []model.OutletResponse{}, total, nil
+	}
+
+	if end > total {
+		end = total
+	}
+
+	paged := filteredOutlets[start:end]
+
+	return paged, total, nil
 }
