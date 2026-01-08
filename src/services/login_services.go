b80d0d3fa50b8132434c355108d49ba2c4611524
septiandr <sdwirisanggalih@gmail.com>
2026-01-08 14:39:21 +0700

    fix login services

--- src/services/login_services.go
+++ src/services/login_services.go
@@ -7,16 +7,20 @@ import (
 
 func LoginService(ctx context.Context, req model.LoginRequest) (model.LoginResponse, error) {
 
+	var roleID *int = nil
+	var roleName *string = nil
+	var features []string = nil
+
 	response := model.LoginResponse{
-        "uuid": "c28f392b-337c-41eb-8277-03bc0246d89b",
-        "name": "Super Admin",
-        "email": "superadmin@mailinator.com",
-        "is_super_admin": true,
-        "role_id": null,
-        "role_name": null,
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN1cGVyYWRtaW5AbWFpbGluYXRvci5jb20iLCJleHAiOjE3Njc4NTkyODAsImZlYXR1cmVzIjpudWxsLCJpZCI6MSwiaW1hZ2UiOiIiLCJpc19zdXBlcl9hZG1pbiI6dHJ1ZSwibmFtZSI6IlN1cGVyIEFkbWluIiwicm9sZV9pZCI6IiIsInJvbGVfbmFtZSI6IiIsInV1aWQiOiJjMjhmMzkyYi0zMzdjLTQxZWItODI3Ny0wM2JjMDI0NmQ4OWIifQ.DO3JpoHlORLo9v8MfZ3TYBkMpibPlYyEdf0YP6GmTLU",
-        "features": null,
-    },
+		UUID:         "c28f392b-337c-41eb-8277-03bc0246d89b",
+		Name:         "Super Admin",
+		Email:        "superadmin@mailinator.com",
+		IsSuperAdmin: true,
+		RoleID:       roleID,
+		RoleName:     roleName,
+		Token:        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN1cGVyYWRtaW5AbWFpbGluYXRvci5jb20iLCJleHAiOjE3Njc4NTkyODAsImZlYXR1cmVzIjpudWxsLCJpZCI6MSwiaW1hZ2UiOiIiLCJpc19zdXBlcl9hZG1pbiI6dHJ1ZSwibmFtZSI6IlN1cGVyIEFkbWluIiwicm9sZV9pZCI6IiIsInJvbGVfbmFtZSI6IiIsInV1aWQiOiJjMjhmMzkyYi0zMzdjLTQxZWItODI3Ny0wM2JjMDI0NmQ4OWIifQ.DO3JpoHlORLo9v8MfZ3TYBkMpibPlYyEdf0YP6GmTLU",
+		Features:     features,
+	}
 
 	return response, nil
 }
