88f3b1e8c52e3b64a71bf2c5d0a80ae4ead7e727
septiandr <sdwirisanggalih@gmail.com>
2026-01-08 14:21:29 +0700

    routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -8,14 +8,19 @@ import (
 
 func Routes(r *gin.Engine) {
 
-	behaveGroup := r.Group("/api/v1/cms")
-	{
-		behaveGroup.POST("/login", handler.LoginHandler)
-		behaveGroup.GET("/profile", handler.ProfileHandler)
-		behaveGroup.GET("/merchants/:merchant_uuid/outlets", handler.GetOutletsListHandler)
-		behaveGroup.GET("/merchants", handler.GetMerchantsListHanlder)
-		behaveGroup.GET("/doctors", handler.GetDoctorsListHandler)
-		behaveGroup.GET("/doctors/:doctor_uuid/sessions", handler.GetDoctorSessions)
-		behaveGroup.GET("/bookings/:doctor_uuid/booked/:date/count", handler.GetListDoctorBookedHandler)
-	}
+	// base api
+	api := r.Group("/api/v1/cms")
+
+	// ================= AUTH =================
+	api.POST("/login", handler.LoginHandler)
+	api.GET("/profile", handler.ProfileHandler)
+
+	// ================= MERCHANT =================
+	api.GET("/merchants", handler.GetMerchantsListHanlder)
+	api.GET("/merchants/:merchant_uuid/outlets", handler.GetOutletsListHandler)
+
+	// ================= DOCTOR =================
+	api.GET("/doctors", handler.GetDoctorsListHandler)
+	api.GET("/doctors/:doctor_uuid/sessions", handler.GetDoctorSessions)
+	api.GET("/doctors/:doctor_uuid/booked/:date/count", handler.GetListDoctorBookedHandler)
 }
